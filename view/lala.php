f09359366da6c2ce96f28055c578518f537413a6
Alexander0710 <santiagolomas0710@gmail.com>
2017-12-07 18:38:46 -0500

    Initial commit
    Subida Inical de Archivos

---
+++ view/lala.php
@@ -0,0 +1,8 @@
+<?php
+// Imprime ejemplo 'Versión actual de PHP: 5.3.8'
+echo 'Versión actual de PHP: ' . phpinfo();
+
+// Imprime ejemplo '2.0' o nada si la extensión no está habilitada
+echo phpversion('tidy');
+
+
